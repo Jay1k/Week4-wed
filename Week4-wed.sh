8ee3cff15ded33859d2c2847970048c3b6477cb2
Jalil mackey <Jalil.Mackey@hgs.hiddengeniusproject.org>
2018-10-09 17:00:00 -0700

    Week4-Wed

---
+++ Week4-wed.sh
@@ -0,0 +1,14 @@
+cd ~/desktop
+mkdir Week4-Wed
+cd Week4-Wed
+touch index.html
+mkdir CSS
+mkdir img
+cd CSS
+touch style1.css
+touch style2.css
+cd ../
+cd img
+curl -o img1.jpg "https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000"
+curl -o img2.jpg "https://pbs.twimg.com/media/Dm20tOLU8AAYuEH.jpg"
+curl -o img3.jpg "https://pbs.twimg.com/media/DU_2zWLVMAAQWzk.jpg"
